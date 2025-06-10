dd30842f809b708b6ebea40970d07d88ed4a961d
RushoBinnabi <69098829+RushoBinnabi@users.noreply.github.com>
2025-06-10 10:01:28 -0400

    continued work on the implementation.

--- FIleChangerLauncherAndCommands.py
+++ FIleChangerLauncherAndCommands.py
@@ -7,6 +7,7 @@ import tkinter
 folderPath = ""
 oldFileExtension = ""
 newFileExtension = ""
+fileNameAndExtension = ""
 
 def getFoldersButtonFunctionality():
     tkinter.Tk().withdraw()
@@ -18,13 +19,27 @@ def getFoldersButtonFunctionality():
 def changeFileExtensionsButtonFunctionality():
     folderPath = FileChangerGUI.folderLocationInputSource.get()
     oldFileExtension = FileChangerGUI.oldFileExtensionInput.get()
+    newFileExtension = FileChangerGUI.newFileExtensionInput.get()
     FileChangerGUI.oldFileExtensionOutputArea.config(state=tkinter.NORMAL)
     FileChangerGUI.newFileExtensionOutputArea.config(state=tkinter.NORMAL)
     for file in os.listdir(folderPath):
-        if file.endswith(oldFileExtension):
-            print("debug") # continue implementation here.
+        fileNameAndExtension = os.path.splitext(file)
+        if fileNameAndExtension[1] == oldFileExtension:
+            os.rename(fileNameAndExtension[0] + oldFileExtension, fileNameAndExtension[0] + newFileExtension)
+        FileChangerGUI.oldFileExtensionOutputArea.insert(tkinter.END, fileNameAndExtension[0] + oldFileExtension + "\n")
+        FileChangerGUI.newFileExtensionOutputArea.insert(tkinter.END, fileNameAndExtension[0] + newFileExtension + "\n")
     FileChangerGUI.oldFileExtensionOutputArea.config(state=tkinter.DISABLED)
     FileChangerGUI.newFileExtensionOutputArea.config(state=tkinter.DISABLED)
 
 def clearUIButtonFunctionality():
-    print("debug")
+    FileChangerGUI.oldFileExtensionOutputArea.config(state=tkinter.NORMAL)
+    FileChangerGUI.newFileExtensionOutputArea.config(state=tkinter.NORMAL)
+    FileChangerGUI.folderLocationInputSource.config(state=tkinter.NORMAL)
+    FileChangerGUI.oldFileExtensionInput.delete(0, tkinter.END)
+    FileChangerGUI.newFileExtensionInput.delete(0, tkinter.END)
+    FileChangerGUI.oldFileExtensionOutputArea.delete(1.0, tkinter.END)
+    FileChangerGUI.newFileExtensionOutputArea.delete(1.0, tkinter.END)
+    FileChangerGUI.folderLocationInputSource.delete(0, tkinter.END)
+    FileChangerGUI.oldFileExtensionOutputArea.config(state=tkinter.DISABLED)
+    FileChangerGUI.newFileExtensionOutputArea.config(state=tkinter.DISABLED)
+    FileChangerGUI.folderLocationInputSource.config(state=tkinter.DISABLED)
